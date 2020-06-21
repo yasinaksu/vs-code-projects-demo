57b6c92b8fbc1296a19a8794515a61e0e0a97015
Yasin Aksu <yasinaksu88@gmail.com>
2020-06-21 16:23:08 +0300

    Builder pattern example commit

---
+++ ConsoleApps/Builder/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector productDirector = new ProductDirector();
+            StudentProductBuilder studentBuilder = new StudentProductBuilder();
+            TeacherProductBuilder teacherBuilder = new TeacherProductBuilder();
+            Product product = new Product { Name = "Laptop", Price = 3000 };
+            var saledProducForStudent = productDirector.GenerateProduct(studentBuilder, product);
+            var saledProducForTeacher = productDirector.GenerateProduct(teacherBuilder, product);
+
+            Console.WriteLine($"Ürün Adı : {product.Name} Fiyat : {product.Price} Öğrenci İndirimli Fiyatı : {saledProducForStudent.Price}");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Ürün Adı : {product.Name} Fiyat : {product.Price} Öğretmen İndirimli Fiyatı : {saledProducForTeacher.Price}");
+        }
+    }
+
+    class StudentProductBuilder : ProductBuilder
+    {
+        Product _product = new Product();
+        public override Product ApplyDiscount(Product product)
+        {
+            _product.Name=product.Name;
+            _product.Price=product.Price*0.80m;
+            return _product;
+        }
+    }
+
+    class TeacherProductBuilder : ProductBuilder
+    {
+        Product _product = new Product();
+        public override Product ApplyDiscount(Product product)
+        {
+            _product.Name=product.Name;
+            _product.Price=product.Price*0.90m;
+            return _product;
+        }
+    }
+
+    class ProductDirector
+    {
+        public Product GenerateProduct(ProductBuilder builder, Product product)
+        {
+            return builder.ApplyDiscount(product);
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    abstract class ProductBuilder
+    {
+        public abstract Product ApplyDiscount(Product product);
+    }
+}
